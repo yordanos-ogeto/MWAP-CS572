b723a0dd345e5396669da7a48efd23bc6f6c2149
Yordanos Tekle Ogeto <yo4christ2018@gmail.com>
2021-07-24 11:01:46 -0500

    updated

--- W1D5/q4/api/data/db.js
+++ W1D5/q4/api/data/db.js
@@ -1,18 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('./student.data');
+require("./student.data");
 
-mongoose.connect(process.env.CONN_URL + process.env.DB_NAME);
+mongoose.connect(process.env.DB_URL + process.env.DB_NAME, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-mongoose.connection.on('connected', function() { console.log("connected with the database") });
+mongoose.connection.on("connected", function () {
+  console.log("connected with the database");
+});
 
-mongoose.connection.on('disconnected', function() { console.log("disconnected with the database") });
+mongoose.connection.on("disconnected", function () {
+  console.log("disconnected with the database");
+});
 
-mongoose.connection.on('error', function() { console.log("error occured with the database") });
+mongoose.connection.on("error", function () {
+  console.log("error occured with the database");
+});
 
-process.on("SIGINT", function() {
-    mongoose.connection.close(function() {
-        console.log("closed connection");
-        process.exit(0);
-    });
+process.on("SIGINT", function () {
+  mongoose.connection.close(function () {
+    console.log("closed connection");
+    process.exit(0);
+  });
 });
--- W1D5/q4/index.js
+++ W1D5/q4/index.js
@@ -1,20 +1,19 @@
-const express = require('express');
-require('dotenv').config();
-require('./api/data/db');
+const express = require("express");
+require("dotenv").config();
+require("./api/data/db");
 
 const app = express();
 app.use(express.json());
 
-const router = require('./api/router');
+const router = require("./api/router");
 
-app.use((req, res, next) => {
-    console.log(req.method, req.url, req.body);
-    next();
+app.use(function (req, res, next) {
+  console.log(req.method, req.url, req.body);
+  next();
 });
 
-app.use('/api', router);
+app.use("/api", router);
 
-
-const server = app.listen(process.env.PORT, function() {
-    console.info(`listening the ${server.address().port}`)
-})
+const server = app.listen(process.env.PORT, function () {
+  console.log("listening to port " + server.address().port);
+});
