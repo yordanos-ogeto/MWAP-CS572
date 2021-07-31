ce33c309474aeb914228a02fc71471c360b120e0
Yordanos Tekle Ogeto <yo4christ2018@gmail.com>
2021-07-30 22:52:45 -0500

    update

--- W2D10/public/angular-app/job-display/job-display-controller.js
+++ W2D10/public/angular-app/job-display/job-display-controller.js
@@ -1,6 +1,6 @@
 angular.module("myApp").controller("JobDisplayController", jobController);
 
-function jobController(JobDataFactory, $routeParams, $location) {
+function jobController(JobDataFactory, $routeParams) {
   const vm = this;
   JobDataFactory.getOneJob($routeParams.id).then(function (response) {
     vm.jobs = response;
